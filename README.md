7fd96aaf42b2b5efee49beeca248bdfd501364ab
kaamaral1096 <karini.sa@hotmail.com>
2022-11-04 19:39:18 -0300

    formulario 100%

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # formulario-simples-HTML-CSS-JS
-Formulário simples utilizando HTML-CSS-JS para reutilização
+Formulário simples utilizando HTML, Css e Javascript.
+A ideia é criar um formulário da forma mais clean possível, para que possa ser reutilizado posteriormente.
---
+++ index.js
@@ -0,0 +1,48 @@
+function handleSubmit() {
+    const concatenatedResultTag = document.getElementById('concatenated-result');
+    const form = document.querySelector('.form');
+    var peoplesList = [];
+
+    const createPeopleObject = () => {
+        const fullNameValue = form.querySelector('.fullName');
+        const birthDateValue = form.querySelector('.birthDate');
+        const occupationValue = form.querySelector('.occupation');
+        const civilStatusValue = form.querySelector('.civilStatus');
+        const emailValue = form.querySelector('.email');
+        const obj = {
+            fullNameValue: fullNameValue.value,
+            birthDateValue: birthDateValue.value,
+            occupationValue: occupationValue.value,
+            civilStatusValue: civilStatusValue.value,
+            emailValue: emailValue.value
+        };
+        return obj;
+    };
+
+    const createReturnWithListItems = (obj) => {
+        peoplesList.push(obj);
+        return peoplesList.map(
+            (pessoa) => {
+                return `
+                    <div class="line"> 
+                        ${pessoa.fullNameValue},
+                        ${pessoa.birthDateValue},
+                        ${pessoa.occupationValue},
+                        ${pessoa.civilStatusValue},
+                        ${pessoa.emailValue}
+                    </div>
+                `
+            }
+        )
+    };
+
+    const createReturn = (event) => {
+        event.preventDefault();
+        const peopleObject = createPeopleObject();
+        const concatenatedResult = createReturnWithListItems(peopleObject);
+        concatenatedResultTag.innerHTML = concatenatedResult;
+    };
+    //Chama a função CreateReturn que montará a estrutura chamando as demais funções
+    form.addEventListener('submit', createReturn)
+}
+handleSubmit();
